237d016ef71b635f6f3d1012841b884d2e774593
Miguel Angel <alonso.lazcano.ma@gmail.com>
2018-08-31 17:10:51 -0400

    Added QTextStream & QString basic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <QApplication>
-#include <QDebug>
+#include <QTextStream>
+#include <QString>
 #include <iostream>
 
 
+void sample_using_qstring(){
+    QTextStream out(stdout);
+
+    QString a = "love";
+
+    a.append(" pizza!");
+    a.prepend("I ");
+    out << a << endl;
+    out << "The string has " << a.count()
+        << " characters" << endl;
+    out << a.toUpper() << endl;
+    out << a.toLower() << endl;
+}
+
 int main() {
     std::cout << "Qt5 version: " << QT_VERSION_STR << std::endl;
+
+    sample_using_qstring();
+
     return 0;
 }
